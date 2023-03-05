f76c87d4c86430c9c42aceb3248a9dd71bbcedcb
Daniel <dursuaf@gmail.com>
2023-03-05 19:32:44 +0100

    Add friends and enemy functionality

--- src/routers/users.router.ts
+++ src/routers/users.router.ts
@@ -3,6 +3,7 @@ import { UsersController } from '../controllers/users.controllers.js';
 import { UsersMongoRepo } from '../repository/users.mongo.repo.js';
 import createDebug from 'debug';
 import { logged } from '../interceptors/logged.js';
+import { authorized } from '../interceptors/authorized.js';
 const debug = createDebug('W7B:router:users');
 
 // eslint-disable-next-line new-cap
@@ -15,7 +16,15 @@ const controller = new UsersController(repo);
 usersRouter.get('/', logged, controller.getAll.bind(controller));
 usersRouter.post('/register', controller.register.bind(controller));
 usersRouter.post('/login', controller.login.bind(controller));
-usersRouter.patch('/addfriend/:id', controller.addFavs.bind(controller));
-
-// UsersRouter.patch('/changefav/:id', controller.changeFav.bind(controller));
-// usersRouter.patch('/deletefav/:id', controller.deleteFav.bind(controller));
+usersRouter.patch(
+  '/addfriend/:id',
+  logged,
+  authorized,
+  controller.addFriend.bind(controller)
+);
+usersRouter.patch(
+  '/addenemy/:id',
+  logged,
+  authorized,
+  controller.addEnemy.bind(controller)
+);
